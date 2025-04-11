db9ddb6c361790361ceee166cccd2590676e478f
Azad Ayhan <azadayhan50@gmail.com>
2025-04-11 15:11:40 +0300

    How to create Arrays

--- arraysDemo/Main.java
+++ arraysDemo/Main.java
@@ -2,6 +2,7 @@ package arraysDemo;
 
 public class Main {
     public static void main(String[] args) {
+
     String customer1 = "Azad";
     String customer2 = "Savaş";
     String customer3 = "Bahar";
@@ -11,6 +12,24 @@ public class Main {
     System.out.println(customer2);
     System.out.println(customer3);
     System.out.println(customer4);
+    System.out.println("-----------------");
+
+    String[] customers = new String[4];
+    customers[0] = "Azad";
+    customers[1] = "Savaş";
+    customers[2] = "Bahar";
+    customers[3] = "Azra";
+
+    for(int i = 0; i < customers.length; i++) {
+        System.out.println(customers[i]);
+    }
+    System.out.println("-----------------");
+
+
+    for (String customer : customers) {
+        System.out.println(customer);
+    }
+
 }
 
 }
