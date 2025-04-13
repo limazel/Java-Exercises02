d9372ecbb46ff3513dff6c37d0a9b26ffacbdc36
Azad Ayhan <azadayhan50@gmail.com>
2025-04-13 23:04:59 +0300

    Create Class

---
+++ classes/CustomerManager.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class CustomerManager {
+    public void addCustomer() {
+        System.out.println("Customer added.");
+    }
+
+    public void removeCustomer() {
+        System.out.println("Customer removed.");
+    }
+
+    public void updateCustomer() {
+        System.out.println("Customer updated.");
+    }
+}
