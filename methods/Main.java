ce58518c8d81f40b35102a72eff6dd646ee56e99
Azad Ayhan <azadayhan50@gmail.com>
2025-04-13 22:33:48 +0300

    Varianel Arguments

--- methods/Main.java
+++ methods/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int total = sum(10, 20);
         System.out.println("Total: " + total);
+        int total2 = sum2(1,2,3,4,5,6,7,8,9);
+        System.out.println("Total2: " + total2);
     }
 
     public static void add() {
@@ -20,5 +22,14 @@ public class Main {
 
     public static int sum(int number1, int number2) {
         return number1 + number2;
+    }
+
+    public static int sum2(int... numbers) {
+        int sum = 0;
+        for(int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
     }
 }
