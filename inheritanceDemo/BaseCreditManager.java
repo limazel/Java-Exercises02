8216825ab6fe368b8a7091b1a56034ead5ae4c59
Azad Ayhan <azadayhan50@gmail.com>
2025-04-14 02:38:44 +0300

    Inheritance Demo App

---
+++ inheritanceDemo/BaseCreditManager.java
@@ -0,0 +1,7 @@
+package inheritanceDemo;
+
+public class BaseCreditManager {
+    public void calculate() {
+        System.out.println("Credit calculated");
+    }
+}
---
+++ inheritanceDemo/DoctorCreditManager.java
@@ -0,0 +1,5 @@
+package inheritanceDemo;
+
+public class DoctorCreditManager extends BaseCreditManager {
+
+}
