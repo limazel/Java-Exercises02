12a873af715f2e18a547337faf3b4d436819c78d
Azad Ayhan <azadayhan50@gmail.com>
2025-04-15 21:56:05 +0300

    Class with ArrayList

---
+++ collections/Customer.java
@@ -0,0 +1,13 @@
+package collections;
+
+public class Customer {
+    int id;
+    String name;
+    String lastName;
+
+    public Customer(int id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
--- collections/Main.java
+++ collections/Main.java
@@ -4,19 +4,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList list = new ArrayList();
-        list.add("1");
-        list.add(2);
-        list.add(3.0);
-        list.add(3.0);
-        list.add(3.0);
-        list.add(3.0);
+        ArrayList<Customer> customers = new ArrayList<>();
+        customers.add(new Customer(1, "John", "Doe"));
+        customers.add(new Customer(2, "Jane", "Smith"));
+        customers.add(new Customer(3, "Alice", "Johnson"));
 
-        System.out.println(list.get(1));
-        list.remove(1);
-        System.out.println(list.get(1));
-        for(Object i: list) {
-         //   System.out.println(i);
+        for (Customer customer : customers) {
+            System.out.println("ID: " + customer.id + ", Name: " + customer.name + ", Last Name: " + customer.lastName);
         }
     }
 }
