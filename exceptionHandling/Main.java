f1600b507ac31d5e9afac5af9ffc56a0e67d109c
Azad Ayhan <azadayhan50@gmail.com>
2025-04-16 02:16:40 +0300

    Exception Demo

--- exceptionHandling/Main.java
+++ exceptionHandling/Main.java
@@ -5,8 +5,14 @@ public class Main {
         int[] numbers = new int[] {1, 2, 3};
     try {
         System.out.println(numbers[3]);
-    } catch (Exception exception){
+    } catch(StringIndexOutOfBoundsException exception) {
+        System.out.println("String index out of bounds: " + exception.getMessage());
+    } catch (ArrayStoreException exception) {
+        System.out.println("Array store exception: " + exception.getMessage());
+    } catch (ArrayIndexOutOfBoundsException exception) {
         System.out.println(exception.getMessage());
+    } catch (Exception exception) {
+        System.out.println("Array index out of bounds: " + exception.getMessage());
     } finally {
         System.out.println("This block always executes.");
     }
