b558cabbe21694ce345f99a65c4f65962a766aad
Azad Ayhan <azadayhan50@gmail.com>
2025-04-15 17:49:41 +0300

    First Usage

--- interfaces/Main.java
+++ interfaces/Main.java
@@ -2,7 +2,8 @@ package interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        ICustomerDal customerDal = new MySqlCustomerDal();
-        customerDal.add();
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.customerDal = new OracleCustomerDal();
+        customerManager.add();
     }
 }
