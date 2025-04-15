33d315ff96cd7758f9170881cae391969b87a96d
Azad Ayhan <azadayhan50@gmail.com>
2025-04-15 17:36:29 +0300

    Worked with Interfaces

---
+++ interfaces/OracleCustomerDal.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public class OracleCustomerDal implements ICustomerDal, IRepository{
+    @Override
+    public void add() {
+        System.out.println("Oracle added");
+    }
+}
