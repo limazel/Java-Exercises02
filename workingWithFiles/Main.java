47816e667b5eb64001cf5be21a68b35beb04fdad
Azad Ayhan <azadayhan50@gmail.com>
2025-04-16 07:21:16 +0300

    Get File Info

--- workingWithFiles/Main.java
+++ workingWithFiles/Main.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+       getFileInfo();
+    }
+
+    public static void createFile() {
         File file = new File("/Users/azad/Desktop/examples/java-BTK/workingWithFiles/student.txt");
         try {
             if(file.createNewFile()) {
@@ -13,6 +17,19 @@ public class Main {
             }
         } catch (IOException err) {
             err.printStackTrace();
+        }
+    }
+
+    public static void getFileInfo() {
+        File file = new File("/Users/azad/Desktop/examples/java-BTK/workingWithFiles/student.txt");
+        if(file.exists()) {
+            System.out.println("File name: " + file.getName());
+            System.out.println("File path: " + file.getAbsolutePath());
+            System.out.println("File writable: " + file.canWrite());
+            System.out.println("File readable: " + file.canRead());
+            System.out.println("File size in bytes: " + file.length());
+        } else {
+
         }
     }
 }
