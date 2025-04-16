f0abf37f2a4e6a87dbd82cea2f93aea8a0d0e01c
Azad Ayhan <azadayhan50@gmail.com>
2025-04-16 07:45:17 +0300

    Write File

--- workingWithFiles/Main.java
+++ workingWithFiles/Main.java
@@ -1,12 +1,14 @@
 package workingWithFiles;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        readFile();
+        writeFile();
     }
 
     public static void createFile() {
@@ -47,5 +49,18 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void writeFile() {
+            try {
+            BufferedWriter writer = new BufferedWriter( new FileWriter("/Users/azad/Desktop/examples/java-BTK/workingWithFiles/student.txt", true));
+                writer.newLine();
+                writer.write("Bahar");
+                System.out.println("File written.");
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
     }
 }
