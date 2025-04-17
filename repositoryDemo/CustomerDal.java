b8d483a9bb649c8465c05c82f0c6d0ea5e12d37a
Azad Ayhan <azadayhan50@gmail.com>
2025-04-17 09:11:04 +0300

    Generic Class Demo

---
+++ repositoryDemo/CustomerDal.java
@@ -0,0 +1,17 @@
+package repositoryDemo;
+
+public class CustomerDal implements IEntityRepository<Customer> {
+    @Override
+    public void add(Customer customer) {
+
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+    @Override
+    public void update(Customer customer) {
+
+    }
+}
---
+++ repositoryDemo/ProductDal.java
@@ -0,0 +1,18 @@
+package repositoryDemo;
+
+public class ProductDal implements IEntityRepository<Product>{
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public void delete(Product product) {
+
+    }
+
+    @Override 
+    public void update(Product product) {
+        
+    }
+}
